a32a138d9f6af4052c67909caf8a7714d1f0c375
Adrian Hoyos <adrianh_589@hotmail.com>
2019-12-01 15:03:11 -0500

    Ejercicio 1015 realizado

--- UriExercises/src/Exercise1015.java
+++ UriExercises/src/Exercise1015.java
@@ -8,16 +8,18 @@ public class Exercise1015 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		DecimalFormat df = new DecimalFormat("0.0000");
-		
-		double x1 = Double.parseDouble(br.readLine()),
-			   y1 = Double.parseDouble(br.readLine()),
-		       x2 = Double.parseDouble(br.readLine()),
-			   y2 = Double.parseDouble(br.readLine());
-		
-		double formula = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-		
+
+        double[] primerPunto = punto(br),
+                 segundoPunto = punto(br);
+        double formula = Math.sqrt( Math.pow(segundoPunto[0] - primerPunto[0], 2) + Math.pow(segundoPunto[1] - primerPunto[1], 2) );
 		System.out.println(df.format(formula));
 
 	}
+
+	static double[] punto(BufferedReader br) throws IOException {
+        String[] capturar = br.readLine().split(" ");
+        double[] xy = {Double.parseDouble(capturar[0]),  Double.parseDouble(capturar[1])};
+        return xy;
+    }
 
 }
