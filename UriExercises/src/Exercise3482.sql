11e6cebe3c0f163408e4337b65e6e86f2ae99d8c
Adrian Hoyos <33093678+adrianh589@users.noreply.github.com>
2024-01-09 16:40:49 -0500

    Update Exercise3482.sql

--- UriExercises/src/Exercise3482.sql
+++ UriExercises/src/Exercise3482.sql
@@ -1,13 +1,17 @@
-SELECT
+WITH RankedUsers AS (
+    SELECT
         CASE WHEN u1.posts < u2.posts THEN u1.user_id ELSE u2.user_id END AS id,
         CASE WHEN u1.posts < u2.posts THEN u1.user_name ELSE u2.user_name END AS seguidor,
         CASE WHEN u2.posts > u1.posts THEN u2.user_name ELSE u1.user_name END AS seguido
-    FROM followers as f1
-        inner join followers f2
-            on f1.user_id_fk = f2.following_user_id_fk and f1.following_user_id_fk = f2.user_id_fk
-        inner join users as u1
-        on f1.user_id_fk = u1.user_id
-    inner join users as u2
-        on f2.user_id_fk = u2.user_id
-    where f1.user_id_fk < f2.user_id_fk
-    order by id;
+    FROM followers AS f1
+    INNER JOIN followers AS f2
+        ON f1.user_id_fk = f2.following_user_id_fk AND f1.following_user_id_fk = f2.user_id_fk
+    INNER JOIN users AS u1
+        ON f1.user_id_fk = u1.user_id
+    INNER JOIN users AS u2
+        ON f2.user_id_fk = u2.user_id
+    WHERE f1.user_id_fk < f2.user_id_fk
+)
+SELECT seguidor, seguido
+FROM RankedUsers
+ORDER BY 1; -- Ordena por la primera columna en la subconsulta (id)
